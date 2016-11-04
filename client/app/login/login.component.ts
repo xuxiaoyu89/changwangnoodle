922cca2c86b0cdf6b0d3e5afa0816464e7445a3a
Xiaoyu Xu <xuxiaoyu@Xiaoyus-MacBook.fios-router.home>
2016-11-03 23:30:10 -0400

    use formGroup

--- client/app/login/login.component.ts
+++ client/app/login/login.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup
+} from '@angular/forms'
 @Component({
   selector: "login",
   template: require('./login.component.html')
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.loginForm = fb.group({
+      username: ['']
+    });
+  }
+
   onSubmit(form: any): void {
     console.log("you submitted a form: ", form);
   }
