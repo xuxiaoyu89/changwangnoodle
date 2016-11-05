177a027415a2559dc680dbdf1475950b5e1c1d4b
Xiaoyu Xu <xuxiaoyu@Xiaoyus-MacBook.fios-router.home>
2016-11-05 00:25:14 -0400

    add css, add form validator

--- client/app/login/login.component.ts
+++ client/app/login/login.component.ts
@@ -1,23 +1,33 @@
 import { Component } from '@angular/core';
 import {
   FormBuilder,
-  FormGroup
+  FormGroup, 
+  Validators,
+  AbstractControl
 } from '@angular/forms'
 @Component({
   selector: "login",
-  template: require('./login.component.html')
+  template: require('./login.component.html'),
+  styles: [require('./login.component.scss')],
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  username: AbstractControl;
+  password: AbstractControl;
+  submitted: Boolean;
 
   constructor(fb: FormBuilder) {
     this.loginForm = fb.group({
-      username: [''],
-      password: ['']
+      username: ['', Validators.required],
+      password: ['', Validators.required]
     });
+    this.username = this.loginForm.controls['username'];
+    this.password = this.loginForm.controls['password'];
+    this.submitted = false;
   }
 
   onSubmit(form: any): void {
+  	this.submitted = true;
     console.log("you submitted a form: ", form);
   }
 }
