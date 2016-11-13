2ff4a7b72d9d5795b96c27e6f55a793b71578a40
xuxiaoyu89 <xuxiaoyu89@gmail.com>
2016-11-12 22:05:55 -0500

    get database working

--- models/user.js
+++ models/user.js
@@ -7,7 +7,8 @@ module.exports = function (sequelize, DataTypes) {
   }, {
     underscored: true,
     freezeTableName: true,
-    tableName: 'USER'
+    tableName: 'user',
+    timestamps: false
   });
 
   return model;
