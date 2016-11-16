5e65a720f7f0150ca5810b56eb10970a97377f0a
Xiaoyu Xu <xuxiaoyu89@gmail.com>
2016-11-15 23:24:23 -0500

    add child routing, not working

--- client/app/app.router.ts
+++ client/app/app.router.ts
@@ -4,13 +4,21 @@ import { RouterModule } from '@angular/router';
 import { LoginComponent }  from './login/login.component.ts';
 import { SignupComponent }  from './signup/signup.component.ts';
 import { UserListComponent} from './userlist/userlist.component.ts';
+import { UserInfoComponent} from './userinfo/userinfo.component.ts';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: 'login', component: LoginComponent },
       { path: 'signup', component: SignupComponent },
-      { path: 'userlist', component: UserListComponent},
+      { 
+        path: 'userlist', 
+        component: UserListComponent,
+        children: [
+          { path: '', component: UserListComponent }, 
+          { path: 'user/:id', component: UserInfoComponent }
+        ]
+      },
       { path: '', component: LoginComponent}
     ])
   ],
