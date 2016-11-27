9afa3122d202db0e0026bfad1f2465f996d8ccf8
Xiaoyu Xu <xuxiaoyu89@gmail.com>
2016-11-26 23:16:56 -0500

    fix some bug

--- routes/api/signup.js
+++ routes/api/signup.js
@@ -14,7 +14,7 @@ router.post('/signup', (req, res) => {
     }
   }).spread((userModel, created) => {
     if(created) {
-      console.log('new user created');
+      //console.log('new user created');
       let token = TokenService.getAccessToken(userModel.id);
       res.status(200).send({
         "status": "success",
